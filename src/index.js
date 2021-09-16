74163ba9fb7eeacc6a64907370c225024a5348ff
gongchen161 <gong.chen161@gmail.com>
2021-09-15 23:19:49 -0400

    Renamed title, removed favicon and fixed mui theme

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';  
+import { MuiThemeProvider, createTheme } from '@material-ui/core/styles';  
 
 const rgb = 'rgb(48, 128, 188)';
-const theme = createMuiTheme({
+const theme = createTheme({
   palette: {
      primary: {
         light: rgb,
